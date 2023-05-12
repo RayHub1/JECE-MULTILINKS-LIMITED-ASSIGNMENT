78b1c92a9d7235f64fba6f1e5587142da29e7fc5
Your Name <you@example.com>
2023-05-12 09:49:52 -0700

    fith commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-
-  },
-  plugins: [],
+    extend: {
+      inset: {
+        '97': '24.25rem', // add custom value for left utility class
+        '98': '24.5rem', // add custom value for left utility class
+        '99': '24.75rem', // add custom value for left utility class
+      },
+    },
+    plugins: [],
+  }
 }
 
